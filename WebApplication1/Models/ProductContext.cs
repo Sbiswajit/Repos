8f6e49567b7dc90a298e28991b45e6296d2c6748
Biswajit <biswajitpatro888@outlook.com>
2020-04-02 15:30:30 +0530

    Product model

--- WebApplication1/Models/ProductContext.cs
+++ WebApplication1/Models/ProductContext.cs
@@ -1,16 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApplication1.Models
 {
-    public class ProductContext : DbContext
+    public class ProductContext
     {
-        public ProductContext( DbContextOptions options) : base(options)
-        {
-        }
-        public DbSet<Product> products { get; set; }
     }
 }
